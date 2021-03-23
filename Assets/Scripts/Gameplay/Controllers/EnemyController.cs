99c09fbc30110ae718b8f22bad5b89f31458a091
jackie <photonicdog@gmail.com>
2021-03-23 19:50:52 +0000

    feat: Create enemy controller

---
+++ Assets/Scripts/Gameplay/Controllers/EnemyController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts.Gameplay.Controllers
+{
+    public class EnemyController : MonoBehaviour
+    {
+        public int hitpoints;
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if(hitpoints == 0)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
