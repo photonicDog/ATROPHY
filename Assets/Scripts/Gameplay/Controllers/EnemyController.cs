26dc457410379edf02ff03797e513e4bd7fb118d
jackie <photonicdog@gmail.com>
2021-04-27 13:36:36 +0100

    feat: Add functionality for enemy death and bullet drop to enemy controller

--- Assets/Scripts/Gameplay/Controllers/EnemyController.cs
+++ Assets/Scripts/Gameplay/Controllers/EnemyController.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections;
+﻿using Assets.Scripts.Gameplay.Types.Interfaces;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Assets.Scripts.Gameplay.Controllers
@@ -6,21 +8,34 @@ namespace Assets.Scripts.Gameplay.Controllers
     public class EnemyController : MonoBehaviour
     {
         public int hitpoints;
+        public List<IBullet> hitBy;
+        public GameObject bulletPrefab;
 
         // Use this for initialization
         void Start()
         {
-
+            hitBy = new List<IBullet>();
         }
 
         // Update is called once per frame
         void Update()
         {
-            if(hitpoints == 0)
-            {
-                Destroy(gameObject);
-            }
             //Debug.Log($"{gameObject.name}: {hitpoints}");
+        }
+
+        public void OnDeath()
+        {
+            DropBullet();
+            Destroy(gameObject);
+        }
+
+        void DropBullet()
+        {
+            if(hitBy.Count == 1)
+            {
+                bulletPrefab.GetComponent<DroppedBulletController>().isSingle = true;
+                Instantiate(bulletPrefab, transform.position, transform.rotation);
+            }
         }
     }
 }
