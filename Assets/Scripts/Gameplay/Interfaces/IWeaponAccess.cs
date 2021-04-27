8279a3f6486a109205952c5c1c7e8c566f0e59da
jackie <photonicdog@gmail.com>
2021-04-27 13:19:44 +0100

    feat: Create interface for weapon controller access

---
+++ Assets/Scripts/Gameplay/Interfaces/IWeaponAccess.cs
@@ -0,0 +1,14 @@
+﻿using Assets.Scripts.Gameplay.Types.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.Gameplay.Interfaces
+{
+    public interface IWeaponAccess
+    {
+        void CollectBullet(IBullet bullet);
+    }
+}
